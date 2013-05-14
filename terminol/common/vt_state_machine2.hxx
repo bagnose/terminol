988820b6351a376f3c1255546cf4a46638275b7a
David Bryant <bagnose@gmail.com>
2013-05-14 19:31:07 +0930

    More tinkerings on VtStateMachine2 - the upcoming replacement.

--- terminol/common/vt_state_machine2.hxx
+++ terminol/common/vt_state_machine2.hxx
@@ -4,25 +4,29 @@
 #define COMMON__VT_STATE_MACHINE2__H
 
 #include "terminol/common/utf8.hxx"
-#if 0
+#include "terminol/common/support.hxx"
+
 #include <map>
 #include <vector>
 
 #include <stdint.h>
 
-typedef uint8_t Octet;
-
-class StateMachine {
+class VtStateMachine2 {
 public:
     static void initialise();
     static void finalise();
 
     class I_Observer {
     public:
-        virtual void machineNormal(utf8::Seq seq) throw () = 0;
-        virtual void machineCsi(const std::vector<int32_t> & args,
-                                Octet code) throw () = 0;
-        //virtual void machineOsc(
+        virtual void machineNormal(utf8::Seq seq, utf8::Length length) throw () = 0;
+        virtual void machineControl(uint8_t c) throw () = 0;
+        virtual void machineEscape(uint8_t c) throw () = 0;
+        virtual void machineCsi(bool priv,
+                                const std::vector<int32_t> & args,
+                                uint8_t mode) throw () = 0;
+        virtual void machineDcs(const std::vector<uint8_t> & seq) throw () = 0;
+        virtual void machineOsc(const std::vector<std::string> & args) throw () = 0;
+        virtual void machineSpecial(uint8_t special, uint8_t code) throw () = 0;
 
     protected:
         I_Observer() {}
@@ -30,38 +34,69 @@ public:
     };
 
 private:
+    I_Observer & _observer;
+
     enum Name : size_t {
         GROUND,
-        LAST_NAME
+        ESCAPE_INTERMEDIATE,
+        ESCAPE,
+        SOS_PM_APC_STRING,
+        CSI_ENTRY,
+        CSI_PARAM,
+        CSI_IGNORE,
+        CSI_INTERMEDIATE,
+        OSC_STRING,
+        DCS_ENTRY,
+        DCS_PARAM,
+        DCS_IGNORE,
+        DCS_INTERMEDIATE,
+        DCS_PASSTHROUGH
     };
 
+    typedef uint8_t Octet;      // Move into utf8, or ascii?
+
     struct OctetRange {
+        static OctetRange single(Octet octet) {
+            return OctetRange(octet, octet + 1);
+        }
+
+        static OctetRange multi(Octet begin, Octet end) {
+            return OctetRange(begin, end);
+        }
+
         Octet begin;
         Octet end;
 
         bool contains(Octet octet) {
-            return begin <= octet && (end == NUL || octet < end);
+            return begin <= octet && (end == '\0' || octet < end);
         }
+
+    private:
+        OctetRange(Octet begin_, Octet end_) : begin(begin_), end(end_) {}
     };
 
-    typedef void (StateMachine::*Method)();
+    typedef void (VtStateMachine2::*Method)();
+
+    struct Transition {
+        std::vector<OctetRange> matches;
+        Method                  action;
+        Name                    destination;
+    };
 
     struct State {
-        Method      enter;
-        Transitions transitions;
-        Method      exit;
+        Method                  enterAction;
+        std::vector<Transition> transitions;
+        Method                  exitAction;
     };
 
-    static const State      STATES[LAST_NAME];
-    static const Transition TRANSITIONS[];
-
-    Transitions _globalTransitions;
+    static std::vector<Transition> _globalTransitions;
+    static std::vector<State>      _states;
 
 public:
-    StateMachine(I_Observer & observer);
+    VtStateMachine2(I_Observer & observer);
+    ~VtStateMachine2();
 
-    void process(utf8::Seq seq);
+    void consume(utf8::Seq seq, utf8::Length length);
 };
-#endif
 
 #endif // COMMON__VT_STATE_MACHINE__H
