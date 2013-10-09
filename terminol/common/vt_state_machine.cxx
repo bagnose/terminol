104c68e95a30f9ad28384700eaf192464069a7a4
David Bryant <bagnose@gmail.com>
2013-10-09 21:03:12 +0930

    Print name of escape sequence in trace output.

--- terminol/common/vt_state_machine.cxx
+++ terminol/common/vt_state_machine.cxx
@@ -4,7 +4,182 @@
 #include "terminol/common/ascii.hxx"
 #include "terminol/support/escape.hxx"
 
+namespace {
+
+    // [0x30..0x7F)
+    const char * SIMPLE_STR[] = {
+        nullptr,    // '0'              0x30
+        nullptr,    // '1'
+        nullptr,    // '2'
+        nullptr,    // '3'
+        nullptr,    // '4'
+        nullptr,    // '5'
+        nullptr,    // '6'
+        "DECSC",    // '7'
+        //
+        "DECRC",    // '8'
+        nullptr,    // '9'
+        nullptr,    // ':'
+        nullptr,    // ';'
+        nullptr,    // '<'
+        "DECKPAM",  // '='
+        "DECKPNM",  // '>'
+        nullptr,    // '?'
+        //
+        nullptr,    // '@'              0x40
+        nullptr,    // 'A'
+        nullptr,    // 'B'
+        nullptr,    // 'C'
+        "IND",      // 'D'
+        "NEL",      // 'E'
+        nullptr,    // 'F'
+        nullptr,    // 'G'
+        //
+        "HTS",      // 'H'
+        nullptr,    // 'I'
+        nullptr,    // 'J'
+        nullptr,    // 'K'
+        nullptr,    // 'L'
+        "RI",       // 'M'
+        "SS2",      // 'N'
+        "SS3",      // 'O'
+        //
+        nullptr,    // 'P'              0x50
+        nullptr,    // 'Q'
+        nullptr,    // 'R'
+        nullptr,    // 'S'
+        nullptr,    // 'T'
+        nullptr,    // 'U'
+        nullptr,    // 'V'
+        nullptr,    // 'W'
+        //
+        nullptr,    // 'X'
+        nullptr,    // 'Y'
+        "DECID",    // 'Z'
+        nullptr,    // '['
+        nullptr,    // '\'
+        nullptr,    // ']'
+        nullptr,    // '^'
+        nullptr,    // '_'
+        //
+        nullptr,    // '`'              0x60
+        nullptr,    // 'a'
+        nullptr,    // 'b'
+        "RIS",      // 'c'
+        nullptr,    // 'd'
+        nullptr,    // 'e'
+        nullptr,    // 'f'
+        nullptr,    // 'g'
+        //
+        nullptr,    // 'h'
+        nullptr,    // 'i'
+        nullptr,    // 'j'
+        nullptr,    // 'k'
+        nullptr,    // 'l'
+        nullptr,    // 'm'
+        nullptr,    // 'n'
+        nullptr,    // 'o'
+        //
+        nullptr,    // 'p'              0x70
+        nullptr,    // 'q'
+        nullptr,    // 'r'
+        nullptr,    // 's'
+        nullptr,    // 't'
+        nullptr,    // 'u'
+        nullptr,    // 'v'
+        nullptr,    // 'w'
+        //
+        nullptr,    // 'x'
+        nullptr,    // 'y'
+        nullptr,    // 'z'
+        nullptr,    // '{'
+        nullptr,    // '|'
+        nullptr,    // '}'
+        nullptr     // '~'
+    };
+
+    // [0x40..0x80)
+    const char * CSI_STR[] = {
+        "ICH",      // '@'              0x40
+        "CUU",      // 'A'
+        "CUD",      // 'B'
+        "CUF",      // 'C'
+        "CUB",      // 'D'
+        "CNL",      // 'E'
+        "CPL",      // 'F'
+        "CHA",      // 'G'
+        //
+        "CUP",      // 'H'
+        "CHT",      // 'I'
+        "ED",       // 'J'
+        "EL",       // 'K'
+        "IL",       // 'L'
+        "DL",       // 'M'
+        nullptr,    // 'N'      EF
+        nullptr,    // 'O'      EA
+        //
+        "DCH",      // 'P'              0x50
+        nullptr,    // 'Q'      SEE
+        nullptr,    // 'R'      CPR
+        "SU",       // 'S'
+        "SD",       // 'T'
+        nullptr,    // 'U'      NP
+        nullptr,    // 'V'      PP
+        "CTC",      // 'W'
+        //
+        "ECH",      // 'X'
+        nullptr,    // 'Y'      CVT
+        "CBT",      // 'Z'
+        nullptr,    // '['      SRS
+        nullptr,    // '\'      PTX
+        nullptr,    // ']'      SDS
+        nullptr,    // '^'      SIMD
+        nullptr,    // '_'      5F
+        //
+        "HPA",      // '`'              0x60
+        "HPR",      // 'a'
+        "REP",      // 'b'
+        "DA",       // 'c'
+        "VPA",      // 'd'
+        "VPR",      // 'e'
+        "HVP",      // 'f'
+        "TBC",      // 'g'
+        //
+        "SM",       // 'h'
+        nullptr,    // 'i'      MC
+        nullptr,    // 'j'      HPB
+        nullptr,    // 'k'      VPB
+        "RM",       // 'l'
+        "SGR",      // 'm'
+        "DSR",      // 'n'
+        nullptr,    // 'o'      DAQ
+        //
+        nullptr,    // 'p'              0x70
+        "SCA",      // 'q'
+        "STBM",     // 'r'
+        nullptr,    // 's'      save cursor?
+        nullptr,    // 't'      window ops?
+        nullptr,    // 'u'      restore cursor
+        nullptr,    // 'v'
+        nullptr,    // 'w'
+        //
+        nullptr,    // 'x'
+        nullptr,    // 'y'
+        nullptr,    // 'z'
+        nullptr,    // '{'
+        nullptr,    // '|'
+        nullptr,    // '}'
+        nullptr,    // '~'
+        nullptr     // DEL
+    };
+
+} // namespace {anonymous}
+
 std::ostream & operator << (std::ostream & ost, const SimpleEsc & esc) {
+    ASSERT(esc.code >= 0x30 && esc.code < 0x7F,
+           "Simple escape code out of range: " <<
+           std::hex << static_cast<int>(esc.code));
+
     // escape initiator
     ost << "^[";
 
@@ -14,12 +189,22 @@ std::ostream & operator << (std::ostream & ost, const SimpleEsc & esc) {
     }
 
     // code
-    ost << esc.code;
+    auto str = SIMPLE_STR[esc.code - 0x30];
+    if (str) {
+        ost << '(' << str << ')';
+    }
+    else {
+        ost << esc.code;
+    }
 
     return ost;
 }
 
 std::ostream & operator << (std::ostream & ost, const CsiEsc & esc) {
+    ASSERT(esc.mode >= 0x40 && esc.mode < 0x80,
+           "CSI Escape mode out of range: " <<
+           std::hex << static_cast<int>(esc.mode));
+
     // CSI initiator
     ost << "^[[";
 
@@ -40,7 +225,13 @@ std::ostream & operator << (std::ostream & ost, const CsiEsc & esc) {
     }
 
     // mode
-    ost << esc.mode;
+    auto str = CSI_STR[esc.mode - 0x40];
+    if (str) {
+        ost << '(' << str << ')';
+    }
+    else {
+        ost << esc.mode;
+    }
 
     return ost;
 }
