c9aa8a3454a908e10e773aae15c3c3ad1174c32c
David Bryant <bagnose@gmail.com>
2013-11-04 17:02:01 +0930

    Use std::chrono for Timer.

--- terminol/support/time.hxx
+++ terminol/support/time.hxx
@@ -4,80 +4,25 @@
 #define COMMON__TIME__HXX
 
 #include <iosfwd>
+#include <chrono>
 
 #include <stdint.h>
-
-//
-// The difference between two points in time, with nanosecond resolution.
-//
-
-#if 0
-class TimeDelta {
-    int64_t _time;
-    explicit TimeDelta(int64_t nanoSeconds) : _time(nanoSeconds) {}
-public:
-    TimeDelta() : _time(0) {}
-
-    TimeDelta & operator += (TimeDelta t) throw ();
-    TimeDelta & operator -= (TimeDelta t) throw ();
-
-    TimeDelta operator - () const throw ();
-};
-
-std::ostream & operator << (std::ostream & ost, TimeDelta td);
-
-TimeDelta operator + (TimeDelta lhs, TimeDelta rhs);
-TimeDelta operator - (TimeDelta lhs, TimeDelta rhs);
-TimeDelta operator * (double    lhs, TimeDelta rhs);
-TimeDelta operator / (TimeDelta lhs, double    rhs);
-double    operator / (TimeDelta lhs, TimeDelta rhs);
-
-bool operator == (TimeDelta lhs, TimeDelta rhs);
-bool operator != (TimeDelta lhs, TimeDelta rhs);
-bool operator <  (TimeDelta lhs, TimeDelta rhs);
-bool operator <= (TimeDelta lhs, TimeDelta rhs);
-bool operator >  (TimeDelta lhs, TimeDelta rhs);
-bool operator >= (TimeDelta lhs, TimeDelta rhs);
-
-//
-// A point in time represented as nanoseconds since 1970 started.
-//
-
-class TimeStamp {
-    int64_t _time;
-    explicit TimeStamp(int64_t nanoSeconds) : _time(nanoSeconds) {}
-public:
-    TimeStamp() : _time(0) {}
-
-    TimeStamp & operator += (TimeDelta t) throw ();
-    TimeStamp & operator -= (TimeDelta t) throw ();
-};
-
-std::ostream & operator << (std::ostream & ost, TimeStamp ts);
-
-TimeStamp operator + (TimeStamp lhs, TimeDelta rhs);
-TimeStamp operator - (TimeStamp lhs, TimeDelta rhs);
-TimeDelta operator - (TimeStamp lhs, TimeStamp rhs);
-
-bool operator == (TimeStamp lhs, TimeStamp rhs);
-bool operator != (TimeStamp lhs, TimeStamp rhs);
-bool operator <  (TimeStamp lhs, TimeStamp rhs);
-bool operator <= (TimeStamp lhs, TimeStamp rhs);
-bool operator >  (TimeStamp lhs, TimeStamp rhs);
-bool operator >= (TimeStamp lhs, TimeStamp rhs);
-#endif
 
 //
 // A timer that expires.
 //
 
 class Timer {
-    uint32_t _sec;
-    uint32_t _usec;
+    typedef std::chrono::steady_clock Clock;
+    Clock::time_point _endTime;
 
 public:
-    explicit Timer(uint32_t milliseconds);
-    bool expired() const;
+    explicit Timer(uint32_t milliseconds) :
+        _endTime(Clock::now() + std::chrono::duration<int,std::milli>(milliseconds)) {}
+
+    bool expired() const {
+        return std::chrono::steady_clock::now() > _endTime;
+    }
 };
 
 #endif // COMMON__TIME__HXX
