d3bc480d24651bcd285da9e15efc8bfb46352021
David Bryant <bagnose@gmail.com>
2017-10-22 22:12:21 +1100

    Use constexpr

--- terminol/common/ascii.hxx
+++ terminol/common/ascii.hxx
@@ -9,46 +9,46 @@
 #include <vector>
 #include <cstdint>
 
-const uint8_t NUL   = '\x00';  // '\0'
-const uint8_t SOH   = '\x01';
-const uint8_t STX   = '\x02';
-const uint8_t ETX   = '\x03';
-const uint8_t EOT   = '\x04';
-const uint8_t ENQ   = '\x05';
-const uint8_t ACK   = '\x06';
-const uint8_t BEL   = '\x07';  // '\a'
-const uint8_t BS    = '\x08';  // '\b'
-const uint8_t HT    = '\x09';  // '\t'
-const uint8_t LF    = '\x0A';  // '\n'
-const uint8_t VT    = '\x0B';  // '\v'
-const uint8_t FF    = '\x0C';  // '\f'
-const uint8_t CR    = '\x0D';  // '\r'
-const uint8_t SO    = '\x0E';
-const uint8_t SI    = '\x0F';
-const uint8_t DLE   = '\x10';
-const uint8_t DC1   = '\x11';
-const uint8_t DC2   = '\x12';
-const uint8_t DC3   = '\x13';
-const uint8_t DC4   = '\x14';
-const uint8_t NAK   = '\x15';
-const uint8_t SYN   = '\x16';
-const uint8_t ETB   = '\x17';
-const uint8_t CAN   = '\x18';
-const uint8_t EM    = '\x19';
-const uint8_t SUB   = '\x1A';
-const uint8_t ESC   = '\x1B';  // '\E'
-const uint8_t FS    = '\x1C';
-const uint8_t GS    = '\x1D';
-const uint8_t RS    = '\x1E';
-const uint8_t US    = '\x1F';
-const uint8_t SPACE = '\x20';
+constexpr uint8_t NUL   = '\x00';  // '\0'
+constexpr uint8_t SOH   = '\x01';
+constexpr uint8_t STX   = '\x02';
+constexpr uint8_t ETX   = '\x03';
+constexpr uint8_t EOT   = '\x04';
+constexpr uint8_t ENQ   = '\x05';
+constexpr uint8_t ACK   = '\x06';
+constexpr uint8_t BEL   = '\x07';  // '\a'
+constexpr uint8_t BS    = '\x08';  // '\b'
+constexpr uint8_t HT    = '\x09';  // '\t'
+constexpr uint8_t LF    = '\x0A';  // '\n'
+constexpr uint8_t VT    = '\x0B';  // '\v'
+constexpr uint8_t FF    = '\x0C';  // '\f'
+constexpr uint8_t CR    = '\x0D';  // '\r'
+constexpr uint8_t SO    = '\x0E';
+constexpr uint8_t SI    = '\x0F';
+constexpr uint8_t DLE   = '\x10';
+constexpr uint8_t DC1   = '\x11';
+constexpr uint8_t DC2   = '\x12';
+constexpr uint8_t DC3   = '\x13';
+constexpr uint8_t DC4   = '\x14';
+constexpr uint8_t NAK   = '\x15';
+constexpr uint8_t SYN   = '\x16';
+constexpr uint8_t ETB   = '\x17';
+constexpr uint8_t CAN   = '\x18';
+constexpr uint8_t EM    = '\x19';
+constexpr uint8_t SUB   = '\x1A';
+constexpr uint8_t ESC   = '\x1B';  // '\E'
+constexpr uint8_t FS    = '\x1C';
+constexpr uint8_t GS    = '\x1D';
+constexpr uint8_t RS    = '\x1E';
+constexpr uint8_t US    = '\x1F';
+constexpr uint8_t SPACE = '\x20';
 
 // '\x21'..'\x40': '!'..'@'
 // '\x41'..'\x5A': 'A'..'Z'
 //
 // '\x61'..'\x7A': 'a'..'z'
 
-const uint8_t DEL   = '\x7F';
+constexpr uint8_t DEL   = '\x7F';
 
 // Streaming helper.
 struct Char {
